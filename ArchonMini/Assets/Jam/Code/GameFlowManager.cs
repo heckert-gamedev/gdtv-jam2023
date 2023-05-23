f2a2f5c05d01c91a7288b5a39a2c8194409bbb8f
heckert-gamedev <unity.github@nostromo.in-berlin.de>
2023-05-23 21:23:21 +0200

    Tile highlighting material for testing, create a 'Piece' prefab, change player to be the cursor. coerce existing scripts to fill the grid array from pre-existing scene objects and then instantiate pieces for player and opponent

--- ArchonMini/Assets/Jam/Code/GameFlowManager.cs
+++ ArchonMini/Assets/Jam/Code/GameFlowManager.cs
@@ -16,13 +16,13 @@ namespace Jam
 
             // In case the scene is loaded first (for development) and we don't wait,
             // the Core prefab is spawned afterwards and fades to black...
-            Invoke(nameof(FadeInForDebug), .5f);
+            Invoke(nameof(FadeInForDebug), .2f);
         }
 
 
         private void FadeInForDebug()
         {
-            fader.FadeIn(.5f);
+            fader.FadeIn(.05f);
         }
     }
 }
--- ArchonMini/Assets/Scripts/BoardManager.cs
+++ ArchonMini/Assets/Scripts/BoardManager.cs
@@ -7,7 +7,7 @@ namespace Jam
 
     public class BoardManager : MonoBehaviour
     {
-        [SerializeField] private Vector2 _dimensions;
+        [SerializeField] private Vector2Int _dimensions;
         [SerializeField] private GameObject _cell;
         [SerializeField] private BoardCell[,] grid;
         [SerializeField] private GameObject _piece;
@@ -21,19 +21,32 @@ namespace Jam
 
         private List<GameObject> _pieces;
 
+
         private void OnEnable()
         {
-            gridPieces = new int[(int)_dimensions.x, (int)_dimensions.y];
+            gridPieces = new int[_dimensions.x, _dimensions.y];
             CreateBoard();
-
         }
+
 
         private void CreateBoard()
         {
-            grid = new BoardCell[(int)_dimensions.x, (int)_dimensions.y];
-            var xl = (int)_dimensions.x;
-            var yl = (int)_dimensions.y;
+            grid = new BoardCell[_dimensions.x, _dimensions.y];
+            var xl = _dimensions.x;
+            var yl = _dimensions.y;
 
+            Vector2Int cellPos = new Vector2Int();
+
+Debug.Log("CreateBoard");
+            foreach (Transform gridCell in transform)
+            {
+                cellPos.x = (int)gridCell.position.x;
+                cellPos.y = (int)gridCell.position.z;
+                Debug.Log($"pos {cellPos.x}, {cellPos.y}");
+                grid[cellPos.x, cellPos.y] = new BoardCell(null, gridCell.gameObject, _highlight);
+            }
+
+            /*            
             for (int i = 0; i < yl; i++)
             {
                 for (int y = 0; y < xl; y++)
@@ -43,27 +56,38 @@ namespace Jam
                     grid[i, y].cellObject.transform.position = new Vector3((transform.position.x + y) * _cell.transform.localScale.x, 0F, (transform.position.z + i) * _cell.transform.localScale.z);
                 }
             }
+            */
+
+            Debug.Log($"{grid}");
 
             AddPieces();
         }
 
         private void AddPieces()
         {
-            int last = (int)_dimensions.y - 1;
+            int last = _dimensions.y - 1;
 
             for (int i = 0; i < _dimensions.x; i++)
             {
-                grid[0, i].piece = Instantiate(_piece);
-                grid[0, i].piece.transform.parent = gameObject.transform;
-                grid[0, i].piece.transform.position = new Vector3((transform.position.x + i) * _cell.transform.localScale.x, 0.2F, transform.position.z * _cell.transform.localScale.z);
+                Debug.Log($"position {i}, 0 ");
+                Debug.Log($"tile {grid[i,0].cellObject.name}");
+                grid[i, 0].piece = Instantiate(_piece, grid[i, 0].cellObject.transform.position, Quaternion.identity);
+                Debug.Log($"position {i}, {last} ");
+                Debug.Log($" {grid[i,last].cellObject.name}");
+                grid[i, last].piece = Instantiate(_piece, grid[i, last].cellObject.transform.position, Quaternion.identity);
+
+                //grid[0, i].piece.transform.parent = gameObject.transform;
+                //grid[0, i].piece.transform.position = new Vector3((transform.position.x + i) * _cell.transform.localScale.x, 0.2F, transform.position.z * _cell.transform.localScale.z);
             }
 
+            /*
             for (int i = 0; i < _dimensions.y; i++)
             {
                 var p = Instantiate(_piece);
-                p.transform.parent = gameObject.transform;
-                p.transform.position = new Vector3((transform.position.x + i) * _cell.transform.localScale.x, 0.2F, (transform.position.z + last) * _cell.transform.localScale.z);
+                //p.transform.parent = gameObject.transform;
+                //p.transform.position = new Vector3((transform.position.x + i) * _cell.transform.localScale.x, 0.2F, (transform.position.z + last) * _cell.transform.localScale.z);
             }
+            */
         }
 
         private void HighlightPossibleMove()
